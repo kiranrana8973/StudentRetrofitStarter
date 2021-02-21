2ded75d1c0ce99ac74e5f99d56a9125d6744d057
Kiran Rana <kiranrana8973@gmail.com>
2021-02-21 08:21:22 +0545

    Added launcher logo and notification

--- app/src/main/java/com/kiran/student/ui/MapsActivity.kt
+++ app/src/main/java/com/kiran/student/ui/MapsActivity.kt
@@ -23,41 +23,46 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-//    override fun onMapReady(googleMap: GoogleMap) {
-//        mMap = googleMap
-//
-//        mMap.addMarker(
-//            MarkerOptions().position(LatLng(27.7061949, 85.3300394))
-//                .title("Hamro College")
-//                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN))
-//        )
-//
-//        mMap.animateCamera(
-//            CameraUpdateFactory.newLatLngZoom(LatLng(27.7061949, 85.3300394), 17F), 5000, null
-//        )
-//        mMap.uiSettings.isZoomControlsEnabled = true
-//    }
+    // Load single locations
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        lstLatitudeLongitude.add(LatitudeLongitude(27.7061949, 85.3300394, "Hamro college"))
-        lstLatitudeLongitude.add(LatitudeLongitude(27.704675, 85.329471, "Gopal dai ko chatamari"))
 
-        for (location in lstLatitudeLongitude) {
-            mMap.addMarker(
-                MarkerOptions().position(LatLng(location.latitude, location.longitude))
-                    .title(location.markerName)
-                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
-            )
-        }
-//        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(27.704675, 85.329471),15F))
-//        mMap.animateCamera(CameraUpdateFactory.zoomTo(15F),5000,null);
-
+        mMap.addMarker(
+            MarkerOptions().position(LatLng(27.7061949, 85.3300394))
+                .title("Hamro College")
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_CYAN))
+        )
 
         mMap.animateCamera(
-            CameraUpdateFactory.newLatLngZoom(LatLng(27.7061949, 85.3300394),16F), 4000, null
-
+            CameraUpdateFactory.newLatLngZoom(LatLng(27.7061949, 85.3300394), 17F), 5000, null
         )
         mMap.uiSettings.isZoomControlsEnabled = true
     }
+
+
+
+    // Load multiple locations
+//    override fun onMapReady(googleMap: GoogleMap) {
+//        mMap = googleMap
+//        lstLatitudeLongitude.add(LatitudeLongitude(27.7061949, 85.3300394, "Hamro college"))
+//        lstLatitudeLongitude.add(LatitudeLongitude(27.704675, 85.329471, "Gopal dai ko chatamari"))
+//
+//        for (location in lstLatitudeLongitude) {
+//            mMap.addMarker(
+//                MarkerOptions().position(LatLng(location.latitude, location.longitude))
+//                    .title(location.markerName)
+//                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN))
+//            )
+//        }
+////        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(LatLng(27.704675, 85.329471),15F))
+////        mMap.animateCamera(CameraUpdateFactory.zoomTo(15F),5000,null);
+//
+//
+//        mMap.animateCamera(
+//            CameraUpdateFactory.newLatLngZoom(LatLng(27.7061949, 85.3300394),16F), 4000, null
+//
+//        )
+//        mMap.uiSettings.isZoomControlsEnabled = true
+//    }
 }
--- app/src/main/java/com/kiran/student/ui/ViewStudentActivity.kt
+++ app/src/main/java/com/kiran/student/ui/ViewStudentActivity.kt
@@ -1,13 +1,19 @@
 package com.kiran.student.ui
 
+import android.app.PendingIntent
+import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.kiran.student.R
 import com.kiran.student.adapter.StudentAdapter
 import com.kiran.student.model.Student
+import com.kiran.student.notification.NotificationChannels
 import com.kiran.student.repository.StudentRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -38,7 +44,10 @@ class ViewStudentActivity : AppCompatActivity() {
                     studentRepository.insertBulkStudent(this@ViewStudentActivity, response.data!!)
 
                     // get data from room database
-                    val lstStudents = studentRepository.getAllStudentsFromRoom(this@ViewStudentActivity )
+                    val lstStudents =
+                        studentRepository.getAllStudentsFromRoom(this@ViewStudentActivity)
+
+                    showNotification(lstStudents.size)
                     withContext(Main) {
                         recyclerView.adapter =
                             StudentAdapter(this@ViewStudentActivity, lstStudents!!)
@@ -55,4 +64,24 @@ class ViewStudentActivity : AppCompatActivity() {
             }
         }
     }
+
+    private fun showNotification(size: Int) {
+        val notificationManager = NotificationManagerCompat.from(this)
+        val activityIntent = Intent(this, MapsActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, activityIntent, 0)
+
+        val notificationChannels = NotificationChannels(this)
+        notificationChannels.createNotificationChannels()
+
+        val notification = NotificationCompat.Builder(this, notificationChannels.CHANNEL_1)
+            .setSmallIcon(R.drawable.notification)
+            .setContentTitle("My notification")
+            .setContentText("Total number of students are $size")
+            .setColor(Color.BLUE)
+            .setContentIntent(pendingIntent)
+            .build()
+
+        notificationManager.notify(1, notification)
+    }
+
 }
